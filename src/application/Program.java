8a1baad08b553a97494d56cac16839922b28f2f8
Pablo Riguetto <pablopatrickbrasil@gmail.com>
2021-05-17 21:07:45 -0400

    Projeto criado

---
+++ src/application/Program.java
@@ -0,0 +1,16 @@
+package application;
+
+import tabuleiro.Posicao;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Posicao pos = new Posicao(3,5);
+		
+		System.out.println(pos);
+		
+
+	}
+
+}
